d2351c0d39d08fbc7507856bf8ac234997a1ee34
KENV76 <kvargas60361@ufide.ac.cr>
2022-06-25 16:01:46 -0600

    Prueba de git

--- Practica/app/src/main/java/com/example/practica/MainActivity.kt
+++ Practica/app/src/main/java/com/example/practica/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.practica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//prueba
+//prueba 2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
